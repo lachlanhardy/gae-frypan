69e651a0b6bdf06aa6674b813ad2736e64c768ab
Lincoln Stoll <lstoll@lstoll.net>
2009-04-16 00:50:26 +0200

    Quick caching example, for future use.

--- app/frypan.rb
+++ app/frypan.rb
@@ -6,12 +6,17 @@ require 'haml'
 
 # homepage
 get '/' do
-  @results = []
-    
-    @search = Twitter::Search.new('frypan')
-    @search.per_page(100)
-    @search.each do |item|
-     @results << item["text"].gsub(/^@\w[a-z]+\s/, '')
+    if @results = GAE::Memcache['twitter-search-frypan-results']
+    else
+      @results = []
+      @search = Twitter::Search.new('frypan')
+      @search.per_page(100)
+      @search.each do |item|
+        @results << item["text"].gsub(/^@\w[a-z]+\s/, '')
+      end
+      # Note - Items need to be Java Serializable. Strings and simple types work, 
+      # But stuff like arrays need to be converted to java.
+      GAE::Memcache.put('twitter-search-frypan-results', @results.to_java(:String), 120000)
     end
      
     haml :index, :options => {:format => :html5,
