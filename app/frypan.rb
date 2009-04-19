beba2a469e422f04f243b6823a807e867abb3b01
Lachlan Hardy <lachlan@lachstock.com.au>
2009-04-19 22:47:26 +1000

    worked through some issues, but she still doesnoesn't run

--- app/frypan.rb
+++ app/frypan.rb
@@ -22,3 +22,19 @@ get '/' do
     haml :index, :options => {:format => :html5,
                              :attr_wrapper => '"'}
 end
+
+
+# oauth = Twitter::OAuth.new('consumer token', 'consumer secret')
+oauth = Twitter::OAuth.new('GXuTs8jAPGaBAbb1S518Q', 'h9fD09H27tEgQtnqmj93YE1pqP9A1V9ZRKw21w')
+# oauth.authorize_from_access('access token', 'access secret')
+# 
+# client = Twitter::Base.new(oauth)
+# client.friends_timeline.each  { |tweet| puts tweet.inspect }
+# client.user_timeline.each     { |tweet| puts tweet.inspect }
+# client.replies.each           { |tweet| puts tweet.inspect }
+
+
+consumer=oauth.consumer
+t=consumer.get_request_token
+puts t.authorize_url
+
--- config.ru
+++ config.ru
@@ -2,7 +2,7 @@ require 'rubygems'
 require 'sinatra'
  
 root_dir = File.dirname(__FILE__)
- 
+
 set :environment, :production
 set :root, root_dir
 set :app_file, File.join(root_dir, 'frypan.rb')
