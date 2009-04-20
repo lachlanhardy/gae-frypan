b4306f74aef3509811416a911c144ae46eb17d91
Lachlan Hardy <lachlan@lachstock.com.au>
2009-04-20 22:09:40 +1000

    Updating tracking code

--- app/frypan.rb
+++ app/frypan.rb
@@ -22,15 +22,7 @@ end
 
 # oauth = Twitter::OAuth.new('consumer token', 'consumer secret')
 oauth = Twitter::OAuth.new('GXuTs8jAPGaBAbb1S518Q', 'h9fD09H27tEgQtnqmj93YE1pqP9A1V9ZRKw21w')
-# oauth.authorize_from_access('access token', 'access secret')
-# 
-# client = Twitter::Base.new(oauth)
-# client.friends_timeline.each  { |tweet| puts tweet.inspect }
-# client.user_timeline.each     { |tweet| puts tweet.inspect }
-# client.replies.each           { |tweet| puts tweet.inspect }
+consumer = oauth.consumer
+t = consumer.get_request_token
 
-
-consumer=oauth.consumer
-t=consumer.get_request_token
 puts t.authorize_url
-
