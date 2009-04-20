d22b9249ccdda7ace88ee474f5228ba90f754b99
Lachlan Hardy <lachlan@lachstock.com.au>
2009-04-21 00:39:29 +1000

    works in Ruby, can't access filesystem in GAE

--- app/frypan.rb
+++ app/frypan.rb
@@ -5,24 +5,61 @@ if RUBY_PLATFORM == 'java'
 end
 require 'twitter'
 require 'haml'
+require 'pp'
+
+configure do
+  set :sessions, true
+end
 
 # homepage
 get '/' do
-    @results = []
-    @search = Twitter::Search.new('frypan')
-    @search.per_page(100)
-    @search.each do |item|
-      @results << item["text"].gsub(/^@\w[a-z]+\s/, '')
+    
+    # require File.join(File.dirname(__FILE__), '..', 'lib', 'twitter')
+    require File.join(File.dirname(__FILE__), 'helpers', 'config_store')
+    
+    config = ConfigStore.new("config/.twitter")
+    
+    oauth = Twitter::OAuth.new(config['token'], config['secret'])
+    
+    if config['atoken'] && config['asecret']
+      oauth.authorize_from_access(config['atoken'], config['asecret'])
+      twitter = Twitter::Base.new(oauth)
+      pp twitter.friends_timeline
+
+    elsif config['rtoken'] && config['rsecret']  
+      oauth.authorize_from_request(config['rtoken'], config['rsecret'])
+      twitter = Twitter::Base.new(oauth)
+      pp twitter.friends_timeline
+
+      config.update({
+        'atoken'  => oauth.access_token.token,
+        'asecret' => oauth.access_token.secret,
+      }).delete('rtoken', 'rsecret')
+    else
+      config.update({
+        'rtoken'  => oauth.request_token.token,
+        'rsecret' => oauth.request_token.secret,
+      })
+
+      @auth_link = oauth.request_token.authorize_url
+      
     end
-     
+    
+    # @results = []
+    #     @search = Twitter::Search.new('frypan')
+    #     @search.per_page(100)
+    #     @search.each do |item|
+    #       @results << item["text"].gsub(/^@\w[a-z]+\s/, '')
+    #     end
+    
+    
     haml :index, :options => {:format => :html5,
                              :attr_wrapper => '"'}
 end
 
+get '/auth' do
+  # ?oauth_token=I084k3L84UU3xoHncars9gQS9mzl95X9FUE49C6ww&user_id=10706&screen_name=lachlanhardy
+  
+  
+end
 
-# oauth = Twitter::OAuth.new('consumer token', 'consumer secret')
-oauth = Twitter::OAuth.new('GXuTs8jAPGaBAbb1S518Q', 'h9fD09H27tEgQtnqmj93YE1pqP9A1V9ZRKw21w')
-consumer = oauth.consumer
-t = consumer.get_request_token
-
-puts t.authorize_url
